b220ab14072d4bba86f432c2386b9b87e64c926a
Samuel Malomo <malomosamuel5@gmail.com>
2021-01-01 01:45:14 +0100

    caesar done

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,39 +1,36 @@
 const caesar = function (string, shift) {
-    let answer = '';
-    let word = string.split('')
-
-    for (i = 0; i < (word.length - 1); i++) {
-        let char = word[i];
+    let answer = string.split('').map(function (word) {
+        let char = word
         if ((/[a-zA-Z]/).test(char) == true) {
-            answer = answer + shiftChar(char, shift);
+            char = shiftChar(char, shift);
+            return char;
         } else {
-            answer = answer + char;
+            return char;
         }
-    };
-
+    }).join('');
     return answer;
+};
 
-    function shiftChar(char, shift) {
-        let code = char.charCodeAt();
-        let newCode = code + (shift % 24);
-        let letter = '';
-        if ((code >= 65 && code <= 90) && newCode < 65) {
-            newCode = 91 - (65 - newCode);
-            letter = letter + String.fromCharCode(newCode);
-        } else if ((code >= 65 && code <= 90) && newCode > 90) {
-            newCode = 64 + (newCode - 90);
-            letter = letter + String.fromCharCode(newCode);
-        } else if ((code >= 97 && code <= 122) && newCode < 97) {
-            newCode = 123 - (97 - newCode);
-            letter = letter + String.fromCharCode(newCode);
-        } else if ((code >= 97 && code <= 122) && newCode > 122) {
-            newCode = 96 + (newCode - 122);
-            letter = letter + String.fromCharCode(newCode);
-        } else {
-            letter = letter + String.fromCharCode(newCode);
-        }
-        return letter;
-    };
+function shiftChar(char, shift) {
+    let code = char.charCodeAt();
+    let newCode = code + (shift % 26);
+    let letter = '';
+    if ((code >= 65 && code <= 90) && newCode < 65) {
+        newCode = 91 - (65 - newCode);
+        letter = letter + String.fromCharCode(newCode);
+    } else if ((code >= 65 && code <= 90) && newCode > 90) {
+        newCode = 64 + (newCode - 90);
+        letter = letter + String.fromCharCode(newCode);
+    } else if ((code >= 97 && code <= 122) && newCode < 97) {
+        newCode = 123 - (97 - newCode);
+        letter = letter + String.fromCharCode(newCode);
+    } else if ((code >= 97 && code <= 122) && newCode > 122) {
+        newCode = 96 + (newCode - 122);
+        letter = letter + String.fromCharCode(newCode);
+    } else {
+        letter = letter + String.fromCharCode(newCode);
+    }
+    return letter;
 };
 
 module.exports = caesar
