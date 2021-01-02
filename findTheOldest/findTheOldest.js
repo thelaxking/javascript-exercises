dff5a5c6f8242556f43368362b476d24bfdad330
Samuel Ademuyiwa Malomo <malomosamuel5@gmail.com>
2021-01-02 10:33:00 +0100

    findtheoldest done

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-let findTheOldest = function() {
+let findTheOldest = function (people) {
+const age = people.sort(function (a, b) {
+        if (a.yearOfDeath == undefined) {
+            let dateNow = Date.now();
+            a.yearOfDeath = dateNow.getFullYear();
+        } else if (b.yearOfDeath == undefined) {
+            let dateNow = new Date();
+            b.yearOfDeath = dateNow.getFullYear();
+        };
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth);
+    });
 
+    return age[0];
 }
 
 module.exports = findTheOldest
